2cbd74f0f26af5f872a5fa5a1e581df86eaea3d7
Overflow <primeaph@student.21-school.ru>
2025-02-18 15:30:58 +0500

    fix

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,5 @@
-import Navbar from '../components/navigation/Navbar'
-import Footer from '../components/navigation/Footer'
+import Navbar from './navigation/Navbar'
+import Footer from './navigation/Footer'
 import { Outlet } from "react-router-dom";
 
 
